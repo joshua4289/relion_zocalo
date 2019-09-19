c56561f3040a14631009c18f24166fdc0cf2e35f
Joshua Lobo <joshua.jude.lobo@gmail.com>
2019-09-19 12:33:08 +0100

    fixed path logic

--- relion_zo/consumers/stop_relion_from_ispyb.py
+++ relion_zo/consumers/stop_relion_from_ispyb.py
@@ -15,15 +15,13 @@ class Relionstop(CommonService):
     def initializing(self):
         """Subscribe to the per_image_analysis queue. Received messages must be acknowledged.
 
-		"""
+                """
 
         queue_name = "relion.relion_stop"
         self.log.info("queue that is being listened to is %s" % queue_name)
         workflows.recipe.wrap_subscribe(self._transport, queue_name,
                                         self.stop_relion, acknowledgement=True, log_extender=self.extend_log,
                                         allow_non_recipe_messages=True)
-
-
 
     def stop_relion(self, rw, header, message):
         self.log.info("Stop relion through zocalo")
@@ -32,8 +30,6 @@ class Relionstop(CommonService):
             from pathlib2 import Path
         except:
             from pathlib import Path
-
-
 
         ispyb_msg = message['session_path']
 
@@ -48,25 +44,27 @@ class Relionstop(CommonService):
         session_path = ispyb_msg_path.parents[2]
         session_name = session_path.name
 
-        relion_project_dir = Path.joinpath(session_path).joinpath('relion_' + session_name)
+        relion_project_dir = Path.joinpath(session_path).joinpath(
+            'processed').joinpath('relion_' + session_name)
         self.log.info(relion_project_dir)
 
+        run_files = [f for f in os.listdir(
+            str(relion_project_dir)) if re.match(r'RUNNING*', f)]
 
-        run_files = [f for f in os.listdir(str(relion_project_dir)) if re.match(r'RUNNING*', f)]
+        self.log.info("%s has asked to be stopped" % (str(session_path)))
 
-
-
-        self.log.info("%s has asked to be stopped" %(str(session_path)))
-
+        if run_files:
         for r in run_files:
-            file_to_delete = session_path.joinpath('processed').joinpath(relion_project_dir).joinpath(r)
+            file_to_delete = session_path.joinpath(
+                relion_project_dir).joinpath(r)
 
-            if file_to_delete.exists:
-                self.log.info("%s will be removed " %(file_to_delete))
+            if file_to_delete.exists():
+                self.log.info("%s will be removed " % (file_to_delete))
                 try:
                     os.remove(str(file_to_delete))
                 except:
-                    self.log.info("Nothing to delete %s has been deleted " %file_to_delete)
+                    self.log.info(
+                        "Nothing to delete %s has been deleted " % file_to_delete)
 
-            #acknowledge in both cases whether file was deleted or if it doesn't exist
-            self.transport.ack(header)
+        # acknowledge in both cases whether file was deleted or if it doesn't exist
+        self.transport.ack(header)
