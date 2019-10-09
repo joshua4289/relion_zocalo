4563c5d1f88661a8307d45ece477eafad8cd6a29
Joshua Lobo <joshua.jude.lobo@gmail.com>
2019-10-09 16:24:33 +0100

    added link to zocalo project

--- relion_zo/__init__.py
+++ relion_zo/__init__.py
@@ -1,2 +1,8 @@
+"""  this is a set of Relion conssumers that use zocalo to consume messsages 
+         https://pypi.org/project/zocalo/
+    it inegrates with the SynchWeb Diamond Light Source frontend
+
+"""
 
 __version__ ='0.6'
+
