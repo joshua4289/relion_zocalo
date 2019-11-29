4c74583d38ad81bc6e324b50d9200ec4cdadc06c
Joshua Lobo <joshua.jude.lobo@gmail.com>
2019-11-29 11:14:34 +0000

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 import sys
 from setuptools import setup, find_packages
 
-requirements = ["workflows", "zocalo","schedule","ispyb-api==5.4.1"]
+requirements = ["workflows", "zocalo","schedule","ispyb==5.4.1"]
 setup_requirements = []
 
 
@@ -32,7 +32,7 @@ setup(
     packages=find_packages(),
     python_requires='>=3.6.*',
     setup_requires=setup_requirements,
-    version="1.5",
+    version="1.4",
     zip_safe=False,
 )
 
