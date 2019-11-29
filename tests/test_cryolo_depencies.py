bed8a9668af6adedbcba8acd08f1d70e50cec2b8
Joshua Lobo <joshua.jude.lobo@gmail.com>
2019-11-29 11:09:12 +0000

    updated tests to use an in memory dict also updated dependency tests to check python environment

--- tests/test_cryolo_depencies.py
+++ tests/test_cryolo_depencies.py
@@ -1,5 +1,5 @@
 import importlib
-
+import pytest
 def find_module(full_module_name):
     """
     Returns module object if module `full_module_name` can be imported. 
@@ -16,9 +16,19 @@ def find_module(full_module_name):
     
 # this is not a dependency for the zocalo runners . This is specifically
 # a dependency for the cryolo wrappers 
+# 
+def test_cryolo_dependencies():
 
-if __name__ == "__main__":
+    ''' this test is a temporary workaround '''
+
     find_module('gemmi')
     find_module('numpy')
-    find_module('relion_yolo_it')	
+    find_module('relion_yolo_it')
+    find_module('ispyb')
+    
+    import ispyb
+    # the sp needed for EM Buffy is in this particular version it will 
+    # eventually be released as a version instead   
+    assert ispyb.__version__ == "5.4.1"
+
     
--- tests/test_params_as_dict.py
+++ tests/test_params_as_dict.py
@@ -2,21 +2,52 @@
 
 #from relion_zo.consumers.zoc_relion_main_consumer import RelionRunner
 import pytest
-
 from relion_zo.consumers.zoc_relion_main_consumer import RelionRunner
+import json
+
+
 #TODO:find out how to make a fixture because file does not always exist check artifacts
 
 
+@pytest.fixture
+def sample_data():
+    #php_to_python = {true:True,false:False}
+    true = True
+    false = False  
+    
+    sample_data = {
+    "import_images": "/dls/tmp/jtq89441/dls/m02/data/2019/em12345-01/raw/GridSquare_*/Data/*.mrc",
+    "motioncor_gainreference": "/dls/tmp/jtq89441/dls/m02/data/2019/em122345-01/raw/Gain.mrc",
+    "voltage": 200,
+    "Cs": 2.7,
+    "ctffind_do_phaseshift": true ,
+    "angpix": 1.0,
+    "motioncor_doseperframe": 0.5,
+    "stop_after_ctf_estimation": false,
+    "do_class2d": true,
+    "do_class3d": true,
+    "autopick_LoG_diam_max": 10,
+    "autopick_LoG_diam_min": 100,
+    "mask_diameter": 190,
+    "extract_downscale": true,
+    "extract_boxsize": 256,
+    "extract_small_boxsize": 64,
+    "do_second_pass": true,
+    "do_class2d_pass2": true,
+    "do_class3d_pass2": true,
+    "autopick_do_cryolo": true
+    
+    }
+
+    
+    return sample_data
 
 
-def test_user_input():
+def test_user_input(sample_data):
+    ''' testing the user input not the relion-it methods '''        
     r = RelionRunner()
-    json_user_params = r.params_as_dict(user_ip='/dls/tmp/jtq89441/dls/m02/data/2019/em12345-01/.ispyb/processed/relion_msg.json')
-    print(json_user_params)
-
-
-# check for extension of raw and gain images
-
+    json_user_params = sample_data 
+    
     assert 'import_images' in json_user_params
     extension = json_user_params['import_images'].split('/*')[-1]
     supported_extensions = ['.mrc','.tif','.tiff']
@@ -44,3 +75,12 @@ def test_user_input():
 
         if gain_extension:
             assert gain_extension in supported_extensions
+
+
+ 
+def test_box_size_only_even(sample_data):
+    
+    """ relion and EM programs in general hate odd box sizes """     
+    
+    assert sample_data['extract_boxsize'] % 2 == 0
+    assert sample_data['extract_small_boxsize'] % 2 == 0
